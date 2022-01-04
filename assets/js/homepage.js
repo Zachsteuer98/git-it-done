0d3f3e030f649f0915e1538086a1474cebe38f2d
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-04 16:32:16 -0500

    pulled issues from Github API and they are being displayed in the single-repo.html page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,7 +9,9 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     
     //make a request to the url
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl)
+        .then(function(response) {
+        // request was successful
         if (response.ok) {
         response.json().then(function(data) {
             displayRepos(data, user);
@@ -17,7 +19,11 @@ var getUserRepos = function(user) {
     } else {
         alert("Error: Github User Not Found");
     }
-    });
+    })
+    .catch(function(error) {
+    // Notice this .catch() getting chained onto the end of the .then()
+    alert("unable to connect to Github");
+});
 };
 
 var formSubmitHandler = function(event) {
@@ -35,6 +41,11 @@ if (username) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
     // clear old content
 repoContainerEl.textContent = "";
 repoSearchTerm.textContent = searchTerm;
